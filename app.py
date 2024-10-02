68b03561cb76a23e57dce1c13a14933d223299bd
rivolt2022 <97595524+rivolt2022@users.noreply.github.com>
2024-10-02 18:10:52 +0900

    Update app.py

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ def main():
       if user_question:
         docs = knowledge_base.similarity_search(user_question)
         
-        llm = OpenAI()
+        llm = OpenAI(model="gpt-4o-mini")
         chain = load_qa_chain(llm, chain_type="stuff")
         with get_openai_callback() as cb:
           response = chain.run(input_documents=docs, question=user_question)
